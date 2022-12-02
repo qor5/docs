75d99320d65364005b1d39516aa11f2e43d9d1e5
xuxin <xuxinmight@gmail.com>
2022-12-02 16:10:42 +0800

    fix page title

--- docsrc/build/main.go
+++ docsrc/build/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/theplant/docgo"
 	"github.com/qor5/docs/docsrc"
+	"github.com/theplant/docgo"
 )
 
 func main() {
 	docgo.New().
 		Assets("/assets/", docsrc.Assets).
-		MainPageTitle("My Document").
+		MainPageTitle("QOR5 Document").
 		SitePrefix("/docs/").
 		DocTree(docsrc.DocTree...).
 		BuildStaticSite("../docs")
