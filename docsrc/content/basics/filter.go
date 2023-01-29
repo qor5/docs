3d40385bee2a2b796c5c1b4a6515ed3455acec27
Raven <enn.raven@gmail.com>
2023-01-29 16:52:05 +0800

    Add filter type doc to filter.

--- docsrc/content/basics/filter.go
+++ docsrc/content/basics/filter.go
@@ -1,7 +1,7 @@
 package basics
 
 import (
-	"github.com/qor5/docs/docsrc/examples/e21_presents"
+	"github.com/qor5/docs/docsrc/examples/example_basics"
 	"github.com/qor5/docs/docsrc/generated"
 	"github.com/qor5/docs/docsrc/utils"
 	. "github.com/theplant/docgo"
@@ -11,14 +11,36 @@ import (
 var Filter = Doc(
 	Markdown(`
 
-To add a basic filter to the list page
+Assume we have a ~status~ filed in Post. It has 2 possible values, "draft" and "online". If we want to filter posts by its status. We can add a filter like this:
 
-For example:
 `),
 	ch.Code(generated.FilterSample).Language("go"),
-	utils.Demo("Basic filter", e21_presents.PresetsBasicFilterPath+"/customers", "e21_presents/filter.go"),
+	utils.Demo("Basic filter", example_basics.PresetsBasicFilterPath+"/customers", "e21_presents/filter.go"),
+
 	Markdown(`
-	Call ~FilterDataFunc~ on a ~ListingBuilder~
+### QOR5 now supports 8 types of filter option.
+
+## Filter by String
+Set the ~ItemType~ as ~vuetifyx.ItemTypeString~. No ~Options~ needed.
+Under this mode, the filter would work in 2 ways,
+1. the target value equal to the input string
+2. the target value contains the input string
+
+## Filter by Number
+Set the ~ItemType~ as ~vuetifyx.ItemTypeNumber~. No ~Options~ needed.
+Under this mode, the filter would work in 4 ways
+1. the target value equal to the input number
+2. the target value is between the input numbers
+3. the target value is greater than the input number
+4. the target value is less than the input number
+
+TODO:
+	3. SelectItem
+	4. DateItem
+	5. DateRangeItem
+	6. DatetimeRangeItem
+	7. MultipleSelectItem
+	8. LinkageSelectItem
 `),
 ).Title("Filters").
 	Slug("basics/filter")
