594f363fc72f521874969c0e1d731441eb169922
molon <3739161+molon@users.noreply.github.com>
2024-05-24 11:01:30 +0800

    publish_test: flowVersionListDialog dep flowDuplicate

--- docsrc/examples/examples_admin/publish_test/flow_version_list_dialog_test.go
+++ docsrc/examples/examples_admin/publish_test/flow_version_list_dialog_test.go
@@ -10,69 +10,85 @@ import (
 	"github.com/qor5/web/v3/multipartestutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gorm.io/gorm"
 )
+
+type FlowVersionListDialog struct {
+	*FlowDuplicate
+}
 
 func TestFlowVersionListDialog(t *testing.T) {
 	dataSeed.TruncatePut(SQLDB)
 
-	flowVersionListDialog(t, PresetsBuilder, DB)
+	flowVersionListDialog(t, &FlowVersionListDialog{
+		FlowDuplicate: &FlowDuplicate{
+			Flow: &Flow{
+				db: DB, h: PresetsBuilder,
+				ID: "6_2024-05-22-v01",
+			},
+		},
+	})
 }
 
-func flowVersionListDialog(t *testing.T, h http.Handler, _ *gorm.DB) {
-	flowVersionListDialog_Step00_Event_presets_DetailingDrawer(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+func flowVersionListDialog(t *testing.T, f *FlowVersionListDialog) {
+	// duplicate one
+	flowDuplicate(t, f.FlowDuplicate)
+
+	// open duplicate drawer
+	flowVersionListDialog_Step00_Event_presets_DetailingDrawer(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
-	flowVersionListDialog_Step01_Event_presets_OpenListingDialog(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	// open list dialog
+	flowVersionListDialog_Step01_Event_presets_OpenListingDialog(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// online_version tab
-	flowVersionListDialog_Step02_Event_presets_UpdateListingDialog(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step02_Event_presets_UpdateListingDialog(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// named_version tab
-	flowVersionListDialog_Step03_Event_presets_UpdateListingDialog(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step03_Event_presets_UpdateListingDialog(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// all tab
-	flowVersionListDialog_Step04_Event_presets_UpdateListingDialog(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step04_Event_presets_UpdateListingDialog(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// keyword 2025
-	flowVersionListDialog_Step05_Event_presets_UpdateListingDialog(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step05_Event_presets_UpdateListingDialog(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// keyword 2024
-	flowVersionListDialog_Step06_Event_presets_UpdateListingDialog(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step06_Event_presets_UpdateListingDialog(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// select
-	flowVersionListDialog_Step07_Event_publish_eventSelectVersion(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step07_Event_publish_eventSelectVersion(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 
 	// click save
-	flowVersionListDialog_Step08_Event_presets_DetailingDrawer(t, h).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
+	flowVersionListDialog_Step08_Event_presets_DetailingDrawer(t, f).Then(func(t *testing.T, w *httptest.ResponseRecorder, r *http.Request) {
 		// assert.Contains(t, w.Body.String(), "xx")
 	})
 }
 
-func flowVersionListDialog_Step00_Event_presets_DetailingDrawer(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step00_Event_presets_DetailingDrawer(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products").
 		EventFunc("presets_DetailingDrawer").
-		Query("id", "6_2024-05-22-v02").
+		// Query("id", "6_2024-05-22-v02").
+		Query("id", f.DuplicateID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -86,21 +102,23 @@ func flowVersionListDialog_Step00_Event_presets_DetailingDrawer(t *testing.T, h 
 	assert.Nil(t, resp.Data)
 	assert.Equal(t, "setTimeout(function(){ vars.presetsRightDrawer = true }, 100)", resp.RunScript)
 
-	testflow.Validate(t, w, r) // TODO: this flow should dep duplicate
-	// testflow.OpenRightDrawer("WithPublishProduct 6_2024-05-22-v02"),
-
+	testflow.Validate(t, w, r,
+		// testflow.OpenRightDrawer("WithPublishProduct 6_2024-05-22-v02"),
+		testflow.OpenRightDrawer("WithPublishProduct "+f.DuplicateID),
+	)
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step01_Event_presets_OpenListingDialog(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step01_Event_presets_OpenListingDialog(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("presets_OpenListingDialog").
-		Query("select_id", "6_2024-05-22-v02").
+		// Query("select_id", "6_2024-05-22-v02").
+		Query("select_id", f.DuplicateID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -117,17 +135,18 @@ func flowVersionListDialog_Step01_Event_presets_OpenListingDialog(t *testing.T, 
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step02_Event_presets_UpdateListingDialog(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step02_Event_presets_UpdateListingDialog(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("presets_UpdateListingDialog").
 		Query("active_filter_tab", "online_version").
 		Query("f_online_version", "1").
-		Query("f_select_id", "6_2024-05-22-v01").
+		// Query("f_select_id", "6_2024-05-22-v01").
+		Query("f_select_id", f.ID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -144,17 +163,18 @@ func flowVersionListDialog_Step02_Event_presets_UpdateListingDialog(t *testing.T
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step03_Event_presets_UpdateListingDialog(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step03_Event_presets_UpdateListingDialog(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("presets_UpdateListingDialog").
 		Query("active_filter_tab", "named_versions").
 		Query("f_named_versions", "1").
-		Query("f_select_id", "6_2024-05-22-v01").
+		// Query("f_select_id", "6_2024-05-22-v01").
+		Query("f_select_id", f.ID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -171,17 +191,18 @@ func flowVersionListDialog_Step03_Event_presets_UpdateListingDialog(t *testing.T
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step04_Event_presets_UpdateListingDialog(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step04_Event_presets_UpdateListingDialog(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("presets_UpdateListingDialog").
 		Query("active_filter_tab", "all").
 		Query("f_all", "1").
-		Query("f_select_id", "6_2024-05-22-v01").
+		// Query("f_select_id", "6_2024-05-22-v01").
+		Query("f_select_id", f.ID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -198,18 +219,19 @@ func flowVersionListDialog_Step04_Event_presets_UpdateListingDialog(t *testing.T
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step05_Event_presets_UpdateListingDialog(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step05_Event_presets_UpdateListingDialog(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("presets_UpdateListingDialog").
 		Query("active_filter_tab", "all").
 		Query("f_all", "1").
-		Query("f_select_id", "6_2024-05-22-v01").
+		// Query("f_select_id", "6_2024-05-22-v01").
+		Query("f_select_id", f.ID).
 		Query("keyword", "2025").
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -226,18 +248,19 @@ func flowVersionListDialog_Step05_Event_presets_UpdateListingDialog(t *testing.T
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step06_Event_presets_UpdateListingDialog(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step06_Event_presets_UpdateListingDialog(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("presets_UpdateListingDialog").
 		Query("active_filter_tab", "all").
 		Query("f_all", "1").
-		Query("f_select_id", "6_2024-05-22-v01").
+		// Query("f_select_id", "6_2024-05-22-v01").
+		Query("f_select_id", f.ID).
 		Query("keyword", "2024").
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -254,15 +277,16 @@ func flowVersionListDialog_Step06_Event_presets_UpdateListingDialog(t *testing.T
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step07_Event_publish_eventSelectVersion(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step07_Event_publish_eventSelectVersion(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products-version-list-dialog").
 		EventFunc("publish_eventSelectVersion").
-		Query("select_id", "6_2024-05-22-v01").
+		// Query("select_id", "6_2024-05-22-v01").
+		Query("select_id", f.ID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -278,15 +302,16 @@ func flowVersionListDialog_Step07_Event_publish_eventSelectVersion(t *testing.T,
 	return testflow.NewThen(t, w, r)
 }
 
-func flowVersionListDialog_Step08_Event_presets_DetailingDrawer(t *testing.T, h http.Handler) *testflow.Then {
+func flowVersionListDialog_Step08_Event_presets_DetailingDrawer(t *testing.T, f *FlowVersionListDialog) *testflow.Then {
 	r := multipartestutils.NewMultipartBuilder().
 		PageURL("/samples/publish/with-publish-products").
 		EventFunc("presets_DetailingDrawer").
-		Query("id", "6_2024-05-22-v01").
+		// Query("id", "6_2024-05-22-v01").
+		Query("id", f.ID).
 		BuildEventFuncRequest()
 
 	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
+	f.h.ServeHTTP(w, r)
 
 	var resp multipartestutils.TestEventResponse
 	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &resp))
@@ -300,8 +325,9 @@ func flowVersionListDialog_Step08_Event_presets_DetailingDrawer(t *testing.T, h 
 	assert.Nil(t, resp.Data)
 	assert.Equal(t, "setTimeout(function(){ vars.presetsRightDrawer = true }, 100)", resp.RunScript)
 
-	testflow.Validate(t, w, r) // TODO: this flow should dep duplicate
-	// testflow.OpenRightDrawer("WithPublishProduct 6_2024-05-22-v01"),
-
+	testflow.Validate(t, w, r,
+		// testflow.OpenRightDrawer("WithPublishProduct 6_2024-05-22-v01"),
+		testflow.OpenRightDrawer("WithPublishProduct "+f.ID),
+	)
 	return testflow.NewThen(t, w, r)
 }
